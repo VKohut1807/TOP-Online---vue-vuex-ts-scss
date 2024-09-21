96b7b5a12cc7203a911203ed414f395012ed1d32
VKohut1807 <vasylkohut1991@gmail.com>
2024-09-21 17:52:05 +0200

    feat:add_image_to_public__text

--- src/data/homePageData.ts
+++ src/data/homePageData.ts
@@ -10,7 +10,7 @@ export const cardData: Ref<Card[]> = ref([
       "In the fantastical world of Piratia, Lance Voy, a daring and intrepid explorer, navigates treacherous seas and uncharted lands in search of legendary treasures. Known for his unyielding courage and unmatched sailing skills, Lance uncovers ancient secrets and battles fearsome foes, all while forging alliances with mystical creatures and unraveling the mysteries of a forgotten era. His journey is a thrilling quest for both glory and truth, where every wave brings a new challenge and every discovery adds a chapter to his legendary saga.",
     route: "home",
     img: {
-      src: "/src/images/lance-voy.webp",
+      src: "/images/lance-voy.webp",
       alt: "lance voy",
     },
   },
@@ -22,7 +22,7 @@ export const cardData: Ref<Card[]> = ref([
       "Phyllis Crusader, a fierce and cunning pirate with a heart as vast as the ocean, roams the treacherous waters of Piratia. Once a noble knight in a distant land, she turned to piracy after a betrayal that cost her everything. Now, she commands her ship with unparalleled skill, seeking vengeance and treasure while protecting her ragtag crew. Her reputation as both a savior and a fearsome adversary makes her a legend among pirates and a shadowy figure to those who cross her path.",
     route: "home",
     img: {
-      src: "/src/images/phyllis-crusader.webp",
+      src: "/images/phyllis-crusader.webp",
       alt: "phyllis crusader",
     },
   },
@@ -34,7 +34,7 @@ export const cardData: Ref<Card[]> = ref([
       "Ami, the skilled herbalist of Piratia, spent her days in the dense jungle, where the air was thick with the scent of wildflowers and ancient trees. With her knowledge of rare plants, she crafted powerful remedies that could heal even the gravest of wounds. One evening, as the sun set over the horizon, Ami discovered a mysterious glowing herb deep within the jungle. Sensing its immense power, she knew it could either be a cure for the island's growing darkness or a weapon in the wrong hands. Determined, she set off to protect it, unaware of the dangers lurking in the shadows.",
     route: "home",
     img: {
-      src: "/src/images/ami-herbalist.webp",
+      src: "/images/ami-herbalist.webp",
       alt: "ami-herbalist",
     },
   },
